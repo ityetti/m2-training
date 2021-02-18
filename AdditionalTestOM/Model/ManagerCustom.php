bd1b4fb883c652cf381943165c9eef5c12b6e170
Oleksii Borovyk <ityetti@gmail.com>
2021-02-18 23:46:00 +0200

    1.3

---
+++ AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\AdditionalTestOM\Model;
+
+use Training\TestOM\Model\ManagerInterface;
+
+class ManagerCustom implements ManagerInterface
+{
+    public function create()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+}
