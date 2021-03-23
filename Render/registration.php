4082ce651ef412f3e4cba37572533dffbb599453
Oleksii Borovyk <ityetti@gmail.com>
2021-03-23 23:59:08 +0200

    3.9

---
+++ Render/registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Training_Render',
+    __DIR__
+);
